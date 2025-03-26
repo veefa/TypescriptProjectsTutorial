da6b056ab6038a2dad2feb2f0ef882fa57a6f8d7
vefa <44091688+veefa@users.noreply.github.com>
2025-03-26 22:15:19 +0000

    adding todoForm

--- todo-app/src/components/todoform.tsx
+++ todo-app/src/components/todoform.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
+
 
 interface TodoFormProps {
   addTodo: (text: string) => void;
@@ -6,29 +7,59 @@ interface TodoFormProps {
 
 const TodoForm: React.FC<TodoFormProps> = ({ addTodo }) => {
   const [text, setText] = useState("");
-
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = (e:React.FormEvent) => {
     e.preventDefault();
     if (text.trim()) {
       addTodo(text);
-      setText("");
+      setText("")
     }
-  };
-
+  }
   return (
     <form onSubmit={handleSubmit} className="flex justify-center gap-2 mb-4">
-      <input
-        type="text"
+      <input type="text"
         value={text}
         onChange={(e) => setText(e.target.value)}
-        placeholder="Add a new task"
-        className="bg-blue-950 p-3 border border-gray-400 rounded w-full max-w-md text-gray-100"
+        placeholder="add a new task"
+        className="p-3 border-gray-300 rounded w-full max-w-md"
       />
-      <button type="submit" className="bg-blue-950 hover:bg-blue-300 px-8 py-2 rounded text-blue-50">
+      <button type="submit" className="bg-blue-400 hover:bg-blue-900 px-5 py-2 rounded text-gray-100">
         Add
       </button>
     </form>
-  );
+  )
 };
 
+
 export default TodoForm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
