f5462e408388c3929453f30bda8d207bf528aac0
vefa <44091688+veefa@users.noreply.github.com>
2025-03-27 21:55:50 +0000

    Cleaned up template files

---
+++ expense-tracker/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center bg-gray-100 min-h-screen">
+      <h1 className="font-bold text-3xl">Hello, World!</h1>
+    </div>
+  );
+};
+
+export default App;
